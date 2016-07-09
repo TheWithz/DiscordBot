f727c2dcd870a6af78102188fa2565c0f43a2737
TheWithz <nathanwithz@gmail.com>
2016-07-09 08:28:15 -0400

    maybe fixed git handler .

--- src/events/GitHandler.java
+++ src/events/GitHandler.java
@@ -37,12 +37,12 @@ public class GitHandler extends ListenerAdapter {
     public static void startTimer() {
         timer.schedule(new TimerTask() {
             public void run() {
-                if (discordRepo.getPushedAt() != lastCommit) {
+                if (!discordRepo.getPushedAt().equals(lastCommit)) {
                     TextChannel textChannel = RunBot.API.getTextChannelById("147169039049949184");
                     textChannel.sendMessageAsync(":white_check_mark: a new Commit has been pushed to DiscordBot", null);
                     lastCommit = discordRepo.getPushedAt();
                 }
             }
-        }, 0, 3 * 1000); // runs every 5 seconds *i think*
+        }, 0, 10 * 1000);
     }
 }
