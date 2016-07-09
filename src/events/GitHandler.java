fe5ccbf8da28e467f0fb204aeb9b0191905f9f06
TheWithz <nathanwithz@gmail.com>
2016-07-09 10:11:25 -0400

    used SHA1 instead of ID

--- src/events/GitHandler.java
+++ src/events/GitHandler.java
@@ -45,10 +45,11 @@ aa9fd8b Fixed issues when JDA received a create event for a new Private channel.
                     if (!discordRepo.getPushedAt().equals(lastCommit)) {
                         GHCommit commit = discordRepo.listCommits().asList().get(0);
                         TextChannel textChannel = RunBot.API.getTextChannelById("147169039049949184");
-                        textChannel.sendMessageAsync(String.format("***%1$s*** / **%2$s** (%3$s)\n`%4$s` %5$s [%6$s]", commit.getAuthor(),
+                        textChannel.sendMessageAsync(String.format("***%1$s*** / **%2$s** (%3$s)\n`%4$s` %5$s [%6$s]",
+                                                                   commit.getAuthor().getLogin(),
                                                                    discordRepo.getName(),
                                                                    discordRepo.getDefaultBranch(),
-                                                                   discordRepo.getId(),
+                                                                   commit.getSHA1(),
                                                                    commit.getCommitShortInfo().getMessage(),
                                                                    github.getMyself().getName()), null);
                         lastCommit = discordRepo.getPushedAt();
